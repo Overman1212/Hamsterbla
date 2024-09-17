ebbdd7b178f00e4f97df7c328e071f6e4ece3d5a
Mehdi <132815992+Overman1212@users.noreply.github.com>
2024-09-17 17:48:32 +0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.addEventListener('contextmenu', (event) => event.preventDefault());
-    document.addEventListener('copy', (event) => event.preventDefault());
-});
